9dbde832bc3c00c3c287c41afee03f1985d6678a
tgrowden <taylorgrowden@gmail.com>
2016-07-11 16:39:28 -0400

    updates

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,41 +1,40 @@
 var express = require('express'),
-  router = express.Router(),
-  db = require('../models'),
-  config = require('../../config/config'),
-  geocoder = (require('node-geocoder')(config.geocoder)),
-  Forecast = require('forecast'),
-  forecast = new Forecast(config.forecast);
+    router = express.Router(),
+    db = require('../models'),
+    config = require('../../config/config'),
+    geocoder = (require('node-geocoder')(config.geocoder)),
+    Forecast = require('forecast'),
+    forecast = new Forecast(config.forecast);
 
 module.exports = function (app) {
   /**
   * Request coordinates based on city/state
   **/
-  app.post('/api/coords', function(req, res, next) {
-    var address = setReq('address');//req.query.address ? req.query.address : req.body.address ? req.body.address : null;
-    console.log(address);
-    var response = res;
-    geocoder.geocode(address, function(err, res) {
-      if (err) {
-        response.send({err: err});
-      } else {
-        response.send(res);
-      }
+    app.post('/api/coords', function(req, res, next) {
+        var address = setReq('address', req);//req.query.address ? req.query.address : req.body.address ? req.body.address : null;
+        var response = res;
+        geocoder.geocode(address, function(err, res) {
+            if (err) {
+                response.json({err: err});
+            } else {
+                response.json(res);
+            }
+        });
     });
-  });
 
 
-  app.post('/api/weather', function (req, res, next) {
-    var lat = setReq('lat'),
-      long = setReq('long');
-    console.log(req.body);
-    var id = req.body.id;
-    res.send({id: id});
-  });
+    app.post('/api/weather', function (req, res, next) {
+        var lat = setReq('lat', req),
+            long = setReq('long', req);
+        forecast.get([lat, long], function(err, weather) {
+            res.json(weather);
+        });
+    });
 
   // Hacky way to allow either form submissions or query params
-  var setReq = function(name) {
-    var response = req.query[name] ? req.query[name] : req.body[name] ? req.body[name] : null;
-    return response;
-  };
+    var setReq = function(name, req) {
+        var response = req.query[name] ? req.query[name] : req.body[name] ? req.body[name] : null;
+        return response;
+    };
 
 };
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,60 @@
 var app = angular.module('weatherApp', []);
-app.controller('myCtrl', function($scope) {
-  $scope.getCoords = function() {
-    console.log('click');
-  };
-});
-app.directive('button', function() {
-  return {
-    restrict: 'E',
-    link: function(scope, elem, attrs) {
-      if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
-        elem.on('click', function(e){
-          e.preventDefault();
+app.controller('mainCtrl', function($scope, $http) {
+    $scope.address = '289 E main st Frostburg MD';
+    $scope.location = {};
+    $scope.selectedLocation = false;
+    
+    $scope.getCoords = function() {
+        var req = {
+            method: 'POST',
+            url: '/api/coords?address=' + $scope.address
+        };
+        $http(req).then(function(success) {
+            success.err = false;
+            return success;
+        }, 
+        function(err) {
+            err.err = true;
+            return err;
+        }).then(function(data) {
+            if (data.err === false) {
+                $scope.location = data.data;
+            } else {
+                $scope.location = false;
+            }
         });
-      }
-    }
-  };
+        
+    };
+
+    $scope.getWeather = function() {
+        var location = $scope.location[this.key];
+        var req = {
+            method: 'POST',
+            url: '/api/weather?lat=' + location.latitude + '&long=' + location.longitude
+        };
+        $http(req).then(function(success) {
+            success.err = false;
+            return success;
+        }, function(err) {
+            err.err = true;
+            return err;
+        }).then(function(data) {
+            if (data.err === false) {
+                data = data.data;
+                console.log(JSON.stringify(data, null, 2));
+                _(['minutely', 'hourly', 'daily']).forEach(function(item) {
+                    _(data[item].data).forEach(function(i) {
+                        i.formattedTime = moment(i.time * 1000).format('MM/DD/YY HH:mm');
+                    });
+                });
+                console.log(JSON.stringify(data));
+                $scope.weatherData = JSON.stringify(data, null, 2);
+            }
+        });
+        $scope.selectedLocation = true;
+    };
+
+    $scope.setWeather = function(key) {
+        console.log(key);
+    };
 });
